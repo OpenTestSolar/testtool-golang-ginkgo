5b644dc7c1b18cd61f4621d4f54d13afd12f514c
zixingdeng <zixingdeng@tencent.com>
2024-12-04 20:17:35 +0800

    fix

--- pkg/cmdline/util.go
+++ pkg/cmdline/util.go
@@ -1,11 +1,44 @@
 package cmdline
 
 import (
+	"log"
+	"os"
 	"regexp"
 	"strings"
 )
 
+func removeTestCaseLabels(tcNames []string) []string {
+	var replacedNames []string
+	pattern := `\s\[([^\[\]]*)\]$`
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		log.Printf("Failed to replace labels, please check if the regular expression is correct: %v", err)
+		return tcNames
+	}
+	mark := os.Getenv("TESTSOLAR_TTP_LABELMARK")
+	for _, name := range tcNames {
+		replace := false
+		labels := re.FindAllString(name, -1)
+		for _, label := range labels {
+			if mark != "" && strings.Contains(label, mark) {
+				replace = true
+				break
+			}
+		}
+		var replacedName string
+		if replace {
+			replacedName = re.ReplaceAllString(name, "")
+		} else {
+			replacedName = name
+		}
+		replacedNames = append(replacedNames, replacedName)
+	}
+	return replacedNames
+}
+
 func GenTestCaseFocusName(tcNames []string) string {
+	// 传入的用例名中可能包含用例标签，ginkgo focus参数中只能识别用例名，因此需要去除
+	tcNames = removeTestCaseLabels(tcNames)
 	// ginkgo中focus参数需要输入一个正则表达式，因此需要将用例名中和正则表达式相关的字符进行转义
 	var escapedNames []string
 	for _, name := range tcNames {
--- pkg/cmdline/util_test.go
+++ pkg/cmdline/util_test.go
@@ -1,14 +1,28 @@
 package cmdline
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestRemoveTestCaseLabels(t *testing.T) {
+	os.Setenv("TESTSOLAR_TTP_LABELMARK", "label")
+	result := removeTestCaseLabels([]string{"case01 [label01]", "case02 [label02]"})
+	assert.Equal(t, result, []string{"case01", "case02"})
+	os.Unsetenv("TESTSOLAR_TTP_LABELMARK")
+	result = removeTestCaseLabels([]string{"case01 [label01]", "case02 [label02]"})
+	assert.Equal(t, result, []string{"case01 [label01]", "case02"})
+}
+
 func TestGenTestCaseFocusName(t *testing.T) {
 	focusName := GenTestCaseFocusName([]string{"[(case01)]", "case02"})
 	assert.Equal(t, focusName, "\\[\\(case01\\)\\]|case02")
+	err := os.Setenv("TESTSOLAR_TTP_LABELMARK", "a")
+	assert.Nil(t, err)
+	focusName = GenTestCaseFocusName([]string{"[xxx] [a, b, c]"})
+	assert.Equal(t, focusName, "\\[xxx\\]")
 }
 
 func TestExtractPackPathFromBinFile(t *testing.T) {
