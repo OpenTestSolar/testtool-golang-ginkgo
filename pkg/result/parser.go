d82cd0a78096c683ec66445ac9d62b9574be6f93
zixingdeng <zixingdeng@tencent.com>
2025-06-30 09:52:02 +0800

    fix

--- pkg/result/parser.go
+++ pkg/result/parser.go
@@ -136,12 +136,12 @@ func (p *ResultParser) Parse() ([]*sdkModel.TestResult, error) {
 			Test: &sdkModel.TestCase{
 				Name: name,
 				Attributes: map[string]string{
-					"nameList":    nameList,
-					"label":       labelList,
-					"tags":        labelList,
-					"owner":       owner,
-					"description": description,
-					"name":        specName,
+					"nameList":               nameList,
+					"label":                  labelList,
+					"tags":                   labelList,
+					"owner":                  owner,
+					"description":            description,
+					"testsolar_requests_key": specName,
 				},
 			},
 			StartTime:  spec.StartTime,
